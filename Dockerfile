9455e79045ab0c7a04e413b574725176faf9021d
Chintan <vedraiyani@gmail.com>
2021-06-23 14:42:54 +0530

    dummy

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,8 @@ FROM java:8-jdk
 RUN mkdir /app
 WORKDIR /app
 COPY target/chintan-springboot-prometheus-0.0.1-SNAPSHOT.jar /app
-ENTRYPOINT ["java", "-Djava.security.egd=file:/dev/./urandom", "-jar", "/app/chintan-springboot-prometheus-0.0.1-SNAPSHOT.jar", "--spring.profiles.active=docker"]
+COPY jmx/jmx_prometheus_javaagent-0.14.0.jar /app
+COPY jmx/config.yaml /app
+ENTRYPOINT ["java", "-javaagent:jmx_prometheus_javaagent-0.14.0.jar=8081:config.yaml", "-jar", "/app/chintan-springboot-prometheus-0.0.1-SNAPSHOT.jar"]
 EXPOSE 8080
+EXPOSE 8081
--- src/main/java/chintan/springbootprometheus/SpringBootPrometheusApplication.java
+++ src/main/java/chintan/springbootprometheus/SpringBootPrometheusApplication.java
@@ -4,14 +4,21 @@ import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
 import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnablePrometheusEndpoint
 @EnableSpringBootMetricsCollector
-
+@RestController
 public class SpringBootPrometheusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPrometheusApplication.class, args);
+	}
+
+	@RequestMapping(value = "/")
+	public String getProductList() {
+	   return "Hello World";
 	}
 }
